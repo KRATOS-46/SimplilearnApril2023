4f0a86717549021750bceb0ab234be156fae4502
unknown <bhuvanesh461998@gmail.com>
2023-05-01 22:20:36 +0530

    first commit

---
+++ CountDemo.java
@@ -0,0 +1,17 @@
+package mypack1;
+
+import java.util.Scanner;
+
+public class CountDemo {
+	public static void main(String[] args) {
+		/*
+		 * Scanner sc = new Scanner(System.in); StringBuilder a = new
+		 * StringBuilder("Happy"); System.out.println(a.delete(0, 1));
+		 */
+		//System.out.println((long)(Math.random()*1000000));
+		String st = "abc";
+		int n = Integer.parseInt(st);
+		System.out.println(n);
+		
+	}
+}
